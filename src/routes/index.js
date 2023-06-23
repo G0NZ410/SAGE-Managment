fad869599ff3f3c2f3206eeebb9c5fe42f3ada27
G0NZ410 <sandoval.vasquez.gon@gmail.com>
2023-06-23 12:58:16 -0600

    Para corroborar

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,11 @@ import { Router } from "express"
 
 const router = Router()
 
-router.get('/', (req, res) => res.render('index', {title: "Segun es el inicio"}))
+router.get('/', (req, res) => res.render('index', {title: "Segun es el inicio", x: 30}))
 
-router.get('/about', (req, res) => res.render('about'))
+router.get('/about', (req, res) => res.render('about', {titulo: "Segun es el inicio"}))
 
-router.get('/contact', (req, res) => res.render('contact'))
+router.get('/contact', (req, res) => res.render('contact', {lodelinicio: "Segun es el inicio"}))
 
 export default router;
 
